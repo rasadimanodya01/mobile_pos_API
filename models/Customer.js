c88c7dd99a723f0cfe1654a288f7e12fc8514632
rasadimanodya <rasadimanodya177@gmail.com>
2026-03-16 12:42:24 +0530

    2

--- models/Customer.js
+++ models/Customer.js
@@ -1,40 +1,25 @@
 const mongoose = require('mongoose');
-    const bcrypt = require(bcrypts);
-
-    const userSchema = new mongoose.Schema({
+    
+    const CustomerSchema = new mongoose.Schema({
         name:{
             type:String,
             required:[true, 'please add a name'],
             trim:true
         },
-        email:{
+        Address:{
             type:String,
-            required:[true,'Please add an email'],
-            unique:true,
-            lowercase:true,
-            match:[/^w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,'please add a valid email']
+            required:[true,'Please add an address'],
+            
+            
         },
-        password:{
-            type:String,
-            required:[true,'please add a password'],
-            minlength:6
+        salary:{
+            type:Number,
+            required:[true,'please add a salary'],
+            min:0
         }
 
-    },{timestamps:true})
+    },{timestamps:true});
 
-    //encrypt password before login...
-    userSchema.pre('save',async function(next){
-        if(!this.isModified('password')){
-
-        }
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password,salt);
-    });
-
-    // match user entered password to hashed password in database
-    userSchema.methodes.matchPassword= async function(enteredPassword){
-        return await bcrypt.compare(enteredPassword,this.password);
-
-    };
-    module.exports = mongoose.model('User',userSchema);
+    
+    module.exports = mongoose.model('Customer',CustomerSchema);
     
